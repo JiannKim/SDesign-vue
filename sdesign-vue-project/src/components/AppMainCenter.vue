5b8a28b33b77fab7de6224598a23c3f604c46d29
JiannKim <jiahnkimm@gmail.com>
2020-10-07 20:07:09 +0900

    css코드 붙이기, 클래스네임을 클래스로 수정

--- sdesign-vue-project/src/components/AppMainCenter.vue
+++ sdesign-vue-project/src/components/AppMainCenter.vue
@@ -55,30 +55,30 @@
         </div>
     </section>
     <!-- 카드섹션 2 -->
-    <section className="Home-content">
+    <section class="Home-content">
         <h1>음향효과 찾아보기</h1>
         <!-- 탭영역 -->
         <!-- 최근 탭 -->
-        <div className="home-categories latest">
-            <div className="browse-container">
+        <div class="home-categories latest">
+            <div class="browse-container">
                 <a href="#">
                     <img src="https://i.ibb.co/8KBMxX2/theme-Park.png" alt="walla sound effects" />
                     <h3><span>theme park</span></h3>
                 </a>
             </div>
-            <div className="browse-container">
+            <div class="browse-container">
                 <a href="#">
                     <img src="https://i.ibb.co/YD33HH5/GFX.png" alt="whoosh sound effects" />
                     <h3><span>gfx animation</span></h3>
                 </a>
             </div>
-            <div className="browse-container">
+            <div class="browse-container">
                 <a href="#">
                     <img src="https://i.ibb.co/6RYyQWc/final-Round.png" alt="walla sound effects" />
                     <h3><span>final round</span></h3>
                 </a>
             </div>
-            <div className="browse-container">
+            <div class="browse-container">
                 <a href="#">
                     <img src="https://i.ibb.co/zVDXFhL/quiet.png" alt="walla sound effects" />
                     <h3><span>quiet streets</span></h3>
@@ -86,26 +86,26 @@
             </div>
         </div>
         <!-- 지역 탭 -->
-        <div className="home-categories location">
-            <div className="browse-container">
+        <div class="home-categories location">
+            <div class="browse-container">
                 <a href="#">
                     <img src="https://i.ibb.co/s5VyDf8/seoul.png" alt="walla sound effects" />
                     <h3><span>서 울</span></h3>
                 </a>
             </div>
-            <div className="browse-container">
+            <div class="browse-container">
                 <a href="#">
                     <img src="https://i.ibb.co/CH6yPL9/shanghai.png" alt="whoosh sound effects" />
                     <h3><span>상 하 이</span></h3>
                 </a>
             </div>
-            <div className="browse-container">
+            <div class="browse-container">
                 <a href="#">
                     <img src="https://i.ibb.co/0sQ6w2r/london.png" alt="walla sound effects" />
                     <h3><span>런 던</span></h3>
                 </a>
             </div>
-            <div className="browse-container">
+            <div class="browse-container">
                 <a href="#">
                     <img src="https://i.ibb.co/qryWT7S/india.png" alt="walla sound effects" />
                     <h3><span>인 도</span></h3>
@@ -113,43 +113,43 @@
             </div>
         </div>
         <!-- 태그 탭 -->
-        <div className="home-categories tags">
-            <div className="tags-container">
+        <div class="home-categories tags">
+            <div class="tags-container">
                 <a href="#">
                     <h3><span>적막한</span></h3>
                 </a>
             </div>
-            <div className="tags-container">
+            <div class="tags-container">
                 <a href="#">
                     <h3><span>시원한</span></h3>
                 </a>
             </div>
-            <div className="tags-container">
+            <div class="tags-container">
                 <a href="#">
                     <h3><span>음악</span></h3>
                 </a>
             </div>
-            <div className="tags-container">
+            <div class="tags-container">
                 <a href="#">
                     <h3><span>신비로운</span></h3>
                 </a>
             </div>
-            <div className="tags-container">
+            <div class="tags-container">
                 <a href="#">
                     <h3><span>습지</span></h3>
                 </a>
             </div>
-            <div className="tags-container">
+            <div class="tags-container">
                 <a href="#">
                     <h3><span>소름끼치는</span></h3>
                 </a>
             </div>
-            <div className="tags-container">
+            <div class="tags-container">
                 <a href="#">
                     <h3><span>광활한</span></h3>
                 </a>
             </div>
-            <div className="tags-container">
+            <div class="tags-container">
                 <a href="#">
                     <h3><span>기계음</span></h3>
                 </a>
@@ -157,40 +157,40 @@
         </div>
     </section>
 
-    <section className="Home-content">
+    <section class="Home-content">
         <h1>사운드 디자이너</h1>
-        <div className="home-categories">
-            <div className="categories-container">
+        <div class="home-categories">
+            <div class="categories-container">
                 <a href="#">
                     <img src="https://i.ibb.co/cvvDS37/a.png" alt="walla sound effects" />
                     <h3><span>aaa</span></h3>
                 </a>
             </div>
-            <div className="categories-container">
+            <div class="categories-container">
                 <a href="#">
                     <img src="https://i.ibb.co/HxvXQBx/b.png" alt="whoosh sound effects" />
                     <h3><span>bbb</span></h3>
                 </a>
             </div>
-            <div className="categories-container">
+            <div class="categories-container">
                 <a href="#">
                     <img src="https://i.ibb.co/HqdF6Qg/c.png" alt="walla sound effects" />
                     <h3><span>ccc</span></h3>
                 </a>
             </div>
-            <div className="categories-container">
+            <div class="categories-container">
                 <a href="#">
                     <img src="https://i.ibb.co/WHp9S14/d.png" alt="walla sound effects" />
                     <h3><span>ddd</span></h3>
                 </a>
             </div>
-            <div className="categories-container">
+            <div class="categories-container">
                 <a href="#">
                     <img src="https://i.ibb.co/JWZ034m/e.png" alt="walla sound effects" />
                     <h3><span>eee</span></h3>
                 </a>
             </div>
-            <div className="categories-container">
+            <div class="categories-container">
                 <a href="#">
                     <img src="https://i.ibb.co/99QX7nb/f.png" alt="walla sound effects" />
                     <h3><span>fff</span></h3>
@@ -206,4 +206,216 @@ export default {};
 </script>
 
 <style>
+.Home-content {
+    padding-top: 77px;
+    width: 1231px;
+    margin: 0 auto;
+    position: relative;
+    top: 73px;
+}
+
+.Home-content h1 {
+    text-align: left;
+    margin: 19px 5px 19px 5px;
+}
+.Home-content img {
+  width: 100%;
+  display: flex;
+}
+.Home-content h3::before {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  content: '\A';
+  background: linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+  /* background: -moz-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+  background: -webkit-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+  background: -webkit-gradient(linear,0 0,0 100%,from(rgba(0,0,0,.8)),to(rgba(16,22,35,.26)));
+  background: -o-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+  background: linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26)); */
+
+}
+
+.home-categories {
+    display: flex;
+    flex-flow: wrap;
+    justify-content: start;
+    width: 100%;
+    margin: 0 auto;
+    /* position: relative; */
+}
+
+.categories-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 5px;
+    width: 195px;
+    height: 140px;
+    border-radius: 5px;
+    background-color: rgba(0, 0, 0, 0.5);
+    overflow: hidden;
+    position: relative;
+    box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.3);
+  }
+
+  .categories-container-wide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 5px;
+    width: 605px;
+    height: 205px;
+    border-radius: 5px;
+    background-color: rgba(0, 0, 0, 0.5);
+    overflow: hidden;
+    position: relative;
+    box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.3);
+  }
+  .home-categories a { width: 100%; }
+
+/* 음향효과 찾아보기 섹션 */
+.browse-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  width: 297px;
+  height: 145px;
+  border-radius: 5px;
+  background-color: rgba(0, 0, 0, 0.5);
+  overflow: hidden;
+  position: relative;
+  box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.3);
+}
+.Home-content h3:hover::before {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  content: '\A';
+  background: #e2ccff50;
+  /* background: linear-gradient(to top,rgba(255, 255, 255, 0.486),#f5f4ff4b); */
+  /* background: -moz-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+  background: -webkit-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+  background: -webkit-gradient(linear,0 0,0 100%,from(rgba(0,0,0,.8)),to(rgba(16,22,35,.26)));
+  background: -o-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+  background: linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26)); */
+}
+
+.home-categories h3 {
+    color: #fff;
+    position: absolute;
+    margin: 0;
+    left: 0;
+    top: 0;
+    text-transform: uppercase;
+    letter-spacing: 5.5px;
+    font-size: 16px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: color 0.03s cubic-bezier(0.8,0.2,0.1,1.6);
+  }
+.home-categories h3:hover {
+  color: #202020;
+  transition: color 0.03s cubic-bezier(0.8,0.2,0.1,1.6);
+  }
+  .home-categories span {
+    z-index: 1;
+    text-indent: 5px;
+  }
+/* 음향효과 찾아보기 탭 */
+  .browse-tabs {
+    display: flex;
+    margin: 10px;
+    width: 200px;
+    justify-content: space-between;
+    font-weight: bold;
+    font-size: 16px;
+  }
+  .tab-title {
+    all: unset;
+    padding: 7px 17px;
+    color: #aaaaaa;
+    cursor: pointer;
+  }
+  .tab-titled {
+    all: unset;
+    padding: 7px 17px;
+    font-weight: bold;
+    color: #202020;
+    border-bottom: 2px solid #1a1a1a;
+  }
+
+  .tags h3 {
+    color: #202020;
+    /* position: absolute;
+    margin: 0;
+    left: 0;
+    top: 0;
+    text-transform: uppercase;
+    letter-spacing: 5.5px;
+    font-size: 16px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+    text-indent: 5px; */
+  }
+  .tags-container h3::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 5px;
+    content: '\A';
+    background: #fbfbff66;
+    /* background: -moz-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+    background: -webkit-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+    background: -webkit-gradient(linear,0 0,0 100%,from(rgba(0,0,0,.8)),to(rgba(16,22,35,.26)));
+    background: -o-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+    background: linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26)); */
+  }
+  .tags-container h3:hover::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 5px;
+    content: '\A';
+    background: #f5f4ff;
+    /* background: linear-gradient(to top,rgba(255, 255, 255, 0.486),#f5f4ff4b); */
+    /* background: -moz-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+    background: -webkit-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+    background: -webkit-gradient(linear,0 0,0 100%,from(rgba(0,0,0,.8)),to(rgba(16,22,35,.26)));
+    background: -o-linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26));
+    background: linear-gradient(to top,rgba(0,0,0,.8),rgba(16,22,35,.26)); */
+  }
+  /* tags container */
+  .tags-container {
+    display: flex;
+    flex-flow: column;
+    /* justify-content: center; */
+    /* align-items: center; */
+    margin: 5px;
+    width: 297px;
+    height: 67px;
+    border-radius: 5px;
+    /* background-color: rgba(0, 0, 0, 0.5); */
+    /* overflow: hidden; */
+    position: relative;
+    box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.07);
+  }
 </style>
