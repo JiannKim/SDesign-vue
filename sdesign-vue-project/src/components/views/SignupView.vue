57d635741eb2589e93b84826f7c66f3a97ace393
JiannKim <jiahnkimm@gmail.com>
2020-10-20 17:49:34 +0900

    회원가입 뷰 페이지

---
+++ sdesign-vue-project/src/components/views/SignupView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="form-container">
+    <SignupForm />
+  </div>
+</template>
+
+<script>
+import SignupForm from "../sign/SignupForm";
+
+export default {
+  components: {
+    SignupForm,
+  },
+};
+</script>
+
+<style>
+@import "../../assets/css/form.css";
+</style>
