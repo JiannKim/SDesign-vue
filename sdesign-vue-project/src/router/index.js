6f4b67730037a1c8ee89efcb2ea781fa9e5236ea
JiannKim <jiahnkimm@gmail.com>
2020-10-22 00:20:55 +0900

    코드 스플리팅으로 폼 라우터 변경

--- sdesign-vue-project/src/router/index.js
+++ sdesign-vue-project/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import SignupView from "@/views/SignupView.vue";
-import LoginView from "@/views/LoginView.vue";
+// import SignupView from "@/views/SignupView.vue";
+// import LoginView from "@/views/LoginView.vue";
 import PricingPage from "@/views/PricingPage.vue";
 import FaqPage from "@/views/FaqPage.vue";
 import SpiPage from "@/views/SpiPage.vue";
@@ -9,15 +9,16 @@ import SpiPage from "@/views/SpiPage.vue";
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: "history",
+  // mode: "history",
   routes: [
     {
       path: "/signup",
-      component: SignupView,
+      // 코드 스플리팅(필요한 페이지를 열때만 데이터를 가져와준다.)
+      component: () => import("@/views/SignupView.vue"),
     },
     {
       path: "/login",
-      component: LoginView,
+      component: () => import("@/views/LoginView.vue"),
     },
     {
       path: "/pricing",
