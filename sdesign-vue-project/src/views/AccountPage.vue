9dbb26ff80a1c87654baa4ff6bc7d3f5de70d2b4
JiannKim <jiahnkimm@gmail.com>
2020-11-23 21:04:55 +0900

    :lipstick: my page 구현중
    포문으로 리스트 랜더링 및 탭 기능 개발중

--- sdesign-vue-project/src/views/AccountPage.vue
+++ sdesign-vue-project/src/views/AccountPage.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="account-page-wrapper">
-    <h1 class="page-title">My Page</h1>
-
-    <div class="upload-section">
-      <h3>Upload List</h3>
-      <hr />
-      <ul>
-        <li>a</li>
-        <li>b</li>
-        <li>c</li>
-      </ul>
+    <div class="top-section">
+      <h1 class="page-title">My Page</h1>
     </div>
-    
+
+    <div class="contents-section">
+      <div class="tab-titles">
+        <ul v-for="tab in tabs" :key="tab.index">
+          <li>{{tab.mainText}}</li>
+        </ul>
+      </div>
+      <template>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+          <li>c</li>
+        </ul>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -20,8 +26,14 @@ import { fetchSounds } from "@/api";
 
 export default {
   data() {
+    const tabs = [
+      { mainText: "Upload" },
+      { mainText: "Profile" },
+      { mainText: "Subscrib" },
+    ];
     return {
-      // key: value,
+      tabs,
+      active: true,
     };
   },
   methods: {
@@ -36,5 +48,32 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+.top-section {
+  padding-bottom: 20px;
+  border-bottom: 1px solid #e1e1e1;
+}
+.contents-section {
+  border: 1px solid aquamarine;
+  .tab-titles {
+    border: 1px solid darkblue;
+    display: flex;
+    justify-content: space-between;
+    max-width: 500px;
+    margin: 0 auto;
+    li {
+      // border: 1px solid palevioletred;
+      width: 100px;
+      padding: 13px;
+      font: {
+        size: 18px;
+        weight: 200;
+      }
+    }
+    li:hover {
+      background-color: #e0e0e0;
+      border-radius: 7px;
+    }
+  }
+}
 </style>
