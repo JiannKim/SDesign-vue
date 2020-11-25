fbc79e3b1fee490d9500ec8320bbe1d494c799fe
JiannKim <jiahnkimm@gmail.com>
2020-11-26 04:34:52 +0900

    :lipstick: upload section 구현중
    레이아웃 잡는중

--- sdesign-vue-project/src/views/AccountPage.vue
+++ sdesign-vue-project/src/views/AccountPage.vue
@@ -16,9 +16,15 @@
         </ul>
       </div>
       <template v-if="selectedTab === tabs[0]">
-        <div>
-          <label for="fname">제목</label>
-          <input type="text" name="fname" id="fname" v-model="fname" />
+        <div class="upload tab-content">
+          <div class="input-container">
+            <label for="fname">제목</label>
+            <input type="text" name="fname" id="fname" v-model="fname" />
+          </div>
+          <div class="input-container">
+            <label for="ftag">태그</label>
+            <input type="text" name="ftag" id="ftag" v-model="ftag" />
+          </div>
         </div>
 
         <!-- 
@@ -30,23 +36,30 @@
         </audio> 
         -->
 
-        <div>
-          <label for="Files">
-            <input type="file" />
+        <div class="upload file-uploader">
+          <label for="files" class="input-label">
+            <input class="me" type="file" id="files" ref="file" />
+            파일찾기
           </label>
-          <!-- <input type="file" /> -->
+          <!-- <p>{{file}}aa.png</p> -->
         </div>
-        <div>
-          <!-- <p>{{file.name}}</p> -->
+        <div class="upload file-submit">
+          <!-- <p>{{file}}</p> -->
           <p>{{fname}}</p>
+          <p>{{ftag}}</p>
           <!-- <span>remove</span> -->
+          <div class="test">
+            <!-- <label for="categories" class="cate" @click="selected()">카테고리</label> -->
+            <fa-icon icon="caret-down" class="caret-icon" />
+            <select name="categories" id="categories" class="input-select-box" ref="selected">
+              <option v-for="option in order" :key="option.index">{{option.text}}</option>
+            </select>
+          </div>
+          <button class="input-submit-button">등록하기</button>
         </div>
         <!-- <div>
           <button>Add</button>
         </div>-->
-        <div>
-          <button>Submit</button>
-        </div>
         <hr />
         <ul>
           <li>a</li>
@@ -73,11 +86,23 @@ export default {
       { mainText: "Upload" },
       { mainText: "Profile" },
       { mainText: "Subscrib" },
+      { mainText: "Favorites" },
+    ];
+    const order = [
+      { text: "카테고리를 선택해주세요", value: "" },
+      { text: "Walla", value: "walla" },
+      { text: "Theme", value: "theme" },
+      { text: "Whoosh", value: "whoosh" },
+      { text: "Wind", value: "wind" },
+      { text: "Cartoon", value: "cartoon" },
     ];
     return {
       tabs,
       selectedTab: "",
       fname: "",
+      ftag: "",
+      fileTitle: "",
+      order,
     };
   },
   methods: {
@@ -88,6 +113,9 @@ export default {
     onClickTab(tab) {
       this.selectedTab = tab;
     },
+    // selected() {
+    //   this.$refs.selected.click();
+    // },
   },
   created() {
     this.fetchData();
@@ -99,28 +127,148 @@ export default {
 <style scoped lang="scss">
 .top-section {
   padding-bottom: 40px;
-  border-bottom: 1px solid #e1e1e1;
 }
 .contents-section {
-  border: 1px solid aquamarine;
-  .tab-titles {
-    border: 1px solid darkblue;
-    max-width: 500px;
+  border: 1px solid olive;
+  .upload {
+    // border: 1px solid hotpink;
+    display: flex;
+    max-width: 521px;
     margin: 0 auto;
+    position: relative;
+    margin-top: 27px;
+    border: 1px solid #e0e0e0;
+    border-radius: 4px;
+  }
+  .tab-titles {
+    border-bottom: 1px solid #e0e0e0;
     ul {
       display: flex;
-      justify-content: space-between;
+      justify-content: space-around;
+      max-width: 664px;
+      margin: 0 auto;
+      position: relative;
       li {
-        // border: 1px solid palevioletred;
         width: 80px;
         padding: 13px;
         font-size: 16px;
+        position: relative;
+        color: #777777;
       }
-      li:hover {
-        background-color: #e0e0e0;
-        border-radius: 7px;
+      .active::after {
+        content: "";
+        width: 100%;
+        height: 2px;
+        background-color: #000;
+        display: block;
+        border-radius: 25px;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+      }
+      li:hover,
+      .active {
+        color: #313131;
       }
     }
+  }
+  .tab-content {
+    // max-width: 521px;
+    // margin: 0 auto;
+    border: none;
+    display: block;
+    margin-top: 28px;
+    .input-container {
+      display: grid;
+      text-align: left;
+      margin-top: 9px;
+      // border: 1px solid green;
+      input {
+        padding: 5px;
+        font-size: 16px;
+        height: 30px;
+        margin-top: 5px;
+        border: 1px solid #e0e0e0;
+        border-radius: 4px;
+      }
+    }
+  }
+  .file-uploader {
+    position: relative;
+    display: flex;
+    align-items: center;
+    // max-width: 521px;
+    // margin: 0 auto;
+    // border: 1px solid #e0e0e0;
+    // border-radius: 4px;
+    .input-label {
+      overflow: hidden;
+      cursor: pointer;
+      width: 90px;
+      height: 100%;
+      padding: 12px 0 10px 0;
+      margin-right: 15px;
+      border-right: 1px solid #e0e0e0;
+      background-image: linear-gradient(to bottom, #f3f3f3, #e0e0e0);
+
+      input[type="file"] {
+        position: absolute;
+        top: 10px;
+        left: 4px;
+        // filter: alpha(opacity=0);
+        // opacity: 0;
+        outline: none;
+        background: white;
+        cursor: inherit;
+        z-index: -1;
+      }
+    }
+    // .input-label:hover {
+    //   background: #ececec;
+    //   transition: none;
+    // }
+  }
+  .test {
+    position: relative;
+    width: 252px;
+    // margin: 0 auto;
+    margin: 0 auto 0 0;
+    // border: 1px solid;
+    .input-select-box {
+      // position: relative;
+      width: 100%;
+      height: 100%;
+      padding: 11px;
+      font-size: 16px;
+      outline: none;
+      border: 1px solid #e0e0e0;
+      border-radius: 4px;
+      background-image: linear-gradient(to bottom, #f3f3f3, #e0e0e0);
+      -webkit-appearance: none; // 네이티브 외형 감추기
+      -moz-appearance: none;
+    }
+    .caret-icon {
+      position: absolute;
+      top: 13px;
+      right: 10px;
+    }
+  }
+  .input-label,
+  .input-select-box:hover {
+    background: #ececec;
+    transition: none;
+  }
+  .input-submit-button {
+    all: unset;
+    width: 103px;
+    padding: 13px 0 10px 0;
+    border: solid 1px #e0e0e0;
+    border-radius: 4px;
+    color: #e0e0e0;
+  }
+  .input-submit-button:hover {
+    border: 1px solid;
+    color: #313131;
   }
 }
 </style>
