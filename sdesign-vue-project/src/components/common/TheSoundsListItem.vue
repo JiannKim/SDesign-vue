9228b877efb3a1adb41172f9be8ab264ef9bc55d
JiannKim <jiahnkimm@gmail.com>
2020-11-27 00:59:51 +0900

    :beers: 컴포넌트 생성
    리스트 아이템 불러와줄 컴포넌트 파일 생성

---
+++ sdesign-vue-project/src/components/common/TheSoundsListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <div>
+      <p>soundName</p>
+      {{listItem.soundName}}
+    </div>
+    <div>
+      <p>category</p>
+      <!-- {{listItem.category}} -->
+    </div>
+    <div>
+      <p>tags</p>
+      <!-- {{listItem.tags}} -->
+    </div>
+    <div>
+      <p>accountName</p>
+      <!-- {{listItem.accountName}} -->
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    listItem: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
