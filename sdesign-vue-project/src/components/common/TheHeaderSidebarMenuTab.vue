2542c0483047cfb4c9bb8917cc48659ccfb49650
JiannKim <jiahnkimm@gmail.com>
2020-12-08 22:32:26 +0900

    :lipstick: 탭 반응 일때 사이드 메뉴 리스트 스타일 수정 / 사운즈 메뉴 추가
    css 코드 수정

--- sdesign-vue-project/src/components/common/TheHeaderSidebarMenuTab.vue
+++ sdesign-vue-project/src/components/common/TheHeaderSidebarMenuTab.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="sidebar-tab-menu-wrapper">
-    <nav class="sidebar-tab-menu-button">
+    <nav class="sidebar-tab-menu">
       <input id="tab-menu-button" type="checkbox" @click="clicked" />
       <!-- 햄버거 메뉴 버튼을 만드는 스펜태그 가운데 스펜은 보이지 않게 한다. -->
       <span></span>
@@ -9,29 +9,38 @@
         <p>menu</p>
       </span>
       <span></span>
-
-      <transition name="listbox">
+      <!-- transition 효과 주기(추후) -->
+      <transition name="listbox-fade">
         <ul
           class="sidebar-tab-menu-lists"
           :class="{ isChecked: ckeckedInput }"
           @click="$emit('close')"
         >
-          <router-link to="/">
-            <li class="tab-menu-lists-list">
+          <li>
+            <router-link to="/" class="tab-menu-lists-list">
               <fa-icon :icon="['fa', 'home']" />
-            </li>
-          </router-link>
-          <router-link to="/pricing">
-            <li class="tab-menu-lists-list">Pricing</li>
-          </router-link>
-          <router-link to="/faq">
-            <li class="tab-menu-lists-list">FAQ</li>
-          </router-link>
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/pricing" class="tab-menu-lists-list">
+              Pricing
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/faq" class="tab-menu-lists-list">
+              FAQ
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/sounds" class="tab-menu-lists-list">
+              Sounds
+            </router-link>
+          </li>
           <!-- 1. 로그인이 되었을 때 -->
           <template v-if="isUserLogin">
-            <p class="tab-menu-lists-list users-info">
+            <p class="users-info isSigned">
               Signed in as
-              <span>{{ $store.state.nickname }}</span>
+              <span class="user-name">{{ $store.state.nickname }}</span>
             </p>
             <router-link to="/account" class="tab-menu-lists-list users-info"
               >My page</router-link
@@ -54,7 +63,6 @@
           </template>
         </ul>
       </transition>
-      <!-- </div> -->
     </nav>
   </div>
 </template>
@@ -74,7 +82,6 @@ export default {
   methods: {
     clicked: function() {
       // console.log("changed");
-      // this.ckeckedInput = true;
       this.ckeckedInput === true
         ? (this.ckeckedInput = false)
         : (this.ckeckedInput = true);
@@ -105,114 +112,94 @@ export default {
   margin: 0 0 0 30px;
   transition: margin 0.5s cubic-bezier(0.4, 0.2, 0.05, 2);
 }
-
-.sidebar-tab-menu-button {
+// menu X button (데스크탑에서 숨기기)
+.sidebar-tab-menu {
   display: none;
   position: relative;
   top: 0px;
   left: 0px;
-
   z-index: 2;
-
-  -webkit-user-select: none;
   user-select: none;
-}
-
-.sidebar-tab-menu-button input {
-  display: block;
-  width: 45px;
-  height: 35px;
-  position: absolute;
-
-  cursor: pointer;
-
-  opacity: 0; /* hide this */
-  z-index: 2; /* and place it over the hamburger */
-
-  -webkit-touch-callout: none;
-}
-
-/* 햄버거 만들기 */
-.sidebar-tab-menu-button span {
-  display: block;
-  width: 35px;
-  height: 4px;
-  margin: 6px 5px 0px 5px;
-  position: relative;
-
-  background: #313131;
-  border-radius: 3px;
-
-  z-index: 1;
-
-  transform-origin: 4px 0px;
-
-  transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
-    background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
-}
-
-/* 메뉴 탭 버튼의 가운데 스팬 숨김 */
-#hidden-span {
-  background-color: #fff;
-  p {
-    font-size: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    top: -4px;
+  -webkit-user-select: none;
+  #tab-menu-button {
+    display: block;
+    width: 45px;
+    height: 35px;
+    position: absolute;
+    cursor: pointer;
+    opacity: 0; /* hide this */
+    z-index: 2; /* and place it over the hamburger */
+    -webkit-touch-callout: none;
   }
 }
-
-.sidebar-tab-menu-button span:first-child {
-  transform-origin: 0% 0%;
+// 햄버거 버튼 공장
+.sidebar-tab-menu {
+  span {
+    display: block;
+    width: 35px;
+    height: 4px;
+    margin: 6px 5px 0px 5px;
+    position: relative;
+    background: #313131;
+    border-radius: 3px;
+    z-index: 1;
+    transform-origin: 4px 0px;
+    transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
+      background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
+  }
+  /* 메뉴 탭 버튼의 가운데 스팬 숨김 */
+  #hidden-span {
+    background-color: #fff;
+    p {
+      font-size: 10px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      top: -4px;
+    }
+  }
+  span {
+    &:first-child {
+      transform-origin: 0% 0%;
+    }
+    &:nth-last-child(2) {
+      transform-origin: 0% 100%;
+    }
+  }
+  // 햄버거 X 변환
+  input:checked ~ span {
+    opacity: 1;
+    transform: rotate(45deg) translate(-2px, -1px);
+    background: cdcdcd;
+    // 가운데 스펜 숨기기
+    &:nth-last-child(3) {
+      opacity: 0;
+      transform: rotate(0deg) scale(0.2, 0.2);
+    }
+    // 마지막 스펜 옮기기
+    &:nth-last-child(2) {
+      transform: rotate(-45deg) translate(0, -1px);
+    }
+  }
 }
-
-.sidebar-tab-menu-button span:nth-last-child(2) {
-  transform-origin: 0% 100%;
-}
-
-/* 햄버거버튼 X자로 변환 */
-.sidebar-tab-menu-button input:checked ~ span {
-  opacity: 1;
-  transform: rotate(45deg) translate(-2px, -1px);
-  background: cdcdcd;
-}
-
-/* 가운데 스펜 숨기기 */
-.sidebar-tab-menu-button input:checked ~ span:nth-last-child(3) {
-  opacity: 0;
-  transform: rotate(0deg) scale(0.2, 0.2);
-}
-
-/* 마지막 스펜 옮기기 */
-.sidebar-tab-menu-button input:checked ~ span:nth-last-child(2) {
-  transform: rotate(-45deg) translate(0, -1px);
-}
-
 /* 기본 위치 왼쪽상단 고정 */
 .sidebar-tab-menu-lists {
-  text-align: left;
   position: fixed;
-  /* width: 150px;
-  height: 226px; */
   width: 50%;
   height: 100%;
   margin: 21px 0 0 -35px;
-  /* padding: 10px 50px 30px 50px; */
   padding: 10px 0px 30px 40px;
   padding-top: 30px;
   background: rgba(255, 255, 255, 0.836);
   box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.1);
+  text-align: left;
   display: flex;
   flex-flow: column;
-  /* justify-content: space-around; */
   list-style-type: none;
   -webkit-font-smoothing: antialiased;
-
   transform-origin: 0% 0%;
   transform: translate(-250%, 0);
-
   transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1);
 }
 /* 파이어폭스 스타일 */
@@ -220,12 +207,9 @@ export default {
   .sidebar-tab-menu-lists {
     text-align: left;
     position: fixed;
-    /* width: 150px;
-  height: 226px; */
     width: 50%;
     height: 100%;
     margin: 20px -50px;
-    /* padding: 10px 50px 30px 50px; */
     padding: 10px 0px 30px 40px;
     padding-top: 30px;
     background: rgba(255, 255, 255, 0.836);
@@ -235,57 +219,56 @@ export default {
     /* justify-content: space-around; */
     list-style-type: none;
     -webkit-font-smoothing: antialiased;
-
     transform-origin: 0% 0%;
     transform: translate(-250%, 0);
-
     transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1);
   }
 
-  .sidebar-tab-menu-lists::before {
-    content: "";
-    background: rgba(0, 0, 0, 0.8);
-    position: absolute;
-    top: 0;
-    left: 20px;
-    width: 20px;
-    height: 100%;
+  // .sidebar-tab-menu-lists::before {
+  //   content: "";
+  //   background: rgba(0, 0, 0, 0.8);
+  //   position: absolute;
+  //   top: 0;
+  //   left: 20px;
+  //   width: 20px;
+  //   height: 100%;
+  // }
+}
+.sidebar-tab-menu-lists {
+  .tab-menu-lists-list {
+    color: rgba(37, 37, 37, 0.7);
+    width: 120px;
+    height: 30px;
+    margin: 10px 0;
+    padding: 3px 0;
+    font-size: 22px;
+    display: flex;
+    &:hover {
+      transform: translateX(5px);
+      color: #313131;
+    }
   }
-}
-
-// list box 왼쪽 검정 막대
-// .sidebar-tab-menu-lists::before {
-//   content: "";
-//   background: rgba(0, 0, 0, 0.8);
-//   position: absolute;
-//   top: 0;
-//   left: 20px;
-//   width: 20px;
-//   height: 100%;
-// }
-
-.sidebar-tab-menu-lists a {
-  text-decoration: none;
-  color: rgba(37, 37, 37, 0.7);
-  width: 120px;
-}
-.tab-menu-lists-list {
-  padding: 10px 0;
-  font-size: 22px;
-  width: 120px;
-  // cursor: pointer;
-}
-.tab-menu-lists-list:hover {
-  transform: translateX(5px);
-  color: #313131;
-}
-.form-list {
-  font-weight: 600;
+  // 로그인 사용자 정보
+  .users-info {
+    width: 135px;
+    font-size: 22px;
+  }
+  .isSigned {
+    width: 90%;
+    margin: 5px 0;
+    .user-name {
+      all: unset;
+      font-weight: 600;
+    }
+  }
+  .form-list {
+    font-weight: 600;
+  }
 }
 
 /* And let's slide it in from the left */
 /* #tab-menu-button:checked ~ #sidebar-tab-menu-lists { */
-/* 위에 있는 ul의 트랜스폼을 넌해주겠다! 나오게 해줌 */
+/* (위에 있는 ul의 트랜스폼을 넌해주겠다) 나오게 해줌 */
 /* transform: none;
 } */
 
@@ -294,23 +277,11 @@ export default {
   transform: none;
 }
 
-// 로그인 사용자 정보
-.users-info {
-  width: 180px;
-  display: flex;
-  align-items: baseline;
-  font-weight: 400;
-  span {
-    font-weight: 600;
-    width: 0;
-  }
-}
-
 /* media Quary */
 
 /* 태블릿 */
 @media (max-width: 1020px) {
-  .sidebar-tab-menu-button {
+  .sidebar-tab-menu {
     display: block !important;
   }
 }
