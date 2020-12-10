c670b39c9357edc565b8e221f12f73feb84004ec
JiannKim <jiahnkimm@gmail.com>
2020-12-11 05:23:58 +0900

    :lipstick: update loading spinner
    로딩 스피너 스타일 수정

--- sdesign-vue-project/src/components/common/LoadingSpinner.vue
+++ sdesign-vue-project/src/components/common/LoadingSpinner.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="spinner-container">
-    <div class="spinner" />
+    <div class="spinner1" />
+    <div class="spinner2" />
+    <div class="spinner3" />
   </div>
 </template>
 
@@ -8,35 +10,87 @@
 export default {};
 </script>
 
-<style scoped lang="scss">
+<style scoped>
 .spinner-container {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 240px;
 }
-.spinner {
+.spinner1 {
   width: 30px;
   height: 30px;
   border-radius: 50%;
   border: 1px solid #e0e0e0;
   animation: spin 1s linear infinite;
   position: relative;
-  &:after {
-    content: "";
-    width: 10px;
-    height: 10px;
-    position: absolute;
-    border-radius: 50%;
-    background: linear-gradient(to bottom, #8546faeb, #5000e4eb);
+}
+.spinner1::after {
+  content: "";
+  width: 8px;
+  height: 8px;
+  position: absolute;
+  top: -3px;
+  border-radius: 50%;
+  background: linear-gradient(to bottom, #8546faeb, #5000e4eb);
+}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
   }
-  @keyframes spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
+  to {
+    transform: rotate(360deg);
+  }
+}
+.spinner2 {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  animation: spinn 1.2s linear infinite;
+  position: relative;
+  top: 0;
+  left: -31px;
+}
+.spinner2::after {
+  content: "";
+  width: 8px;
+  height: 8px;
+  position: absolute;
+  top: -3px;
+  border-radius: 30%;
+  background: linear-gradient(to bottom, #8546faeb, #5000e4eb);
+}
+@keyframes spinn {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(-360deg);
+  }
+}
+.spinner3 {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  animation: spinnn 2s linear infinite;
+  position: relative;
+  top: 0;
+  left: -61px;
+}
+.spinner3::after {
+  content: "";
+  width: 8px;
+  height: 8px;
+  position: absolute;
+  top: -3px;
+  background: linear-gradient(to bottom, #8546faeb, #5000e4eb);
+}
+@keyframes spinnn {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
   }
 }
 </style>
